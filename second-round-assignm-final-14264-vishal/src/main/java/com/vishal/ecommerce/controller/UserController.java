201cf8cc543b4640bdfeb92845ab97aadf7cfbbf
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 00:06:47 +0530

    add UserController with register and login API endpoints

---
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.vishal.ecommerce.controller;
+
+import com.vishal.ecommerce.dto.req.UserLoginReqDto;
+import com.vishal.ecommerce.dto.req.UserRegisterReqDto;
+import com.vishal.ecommerce.dto.res.UserAuthResDto;
+import com.vishal.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserAuthResDto> register(@RequestBody UserRegisterReqDto request) {
+        UserAuthResDto response = userService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserAuthResDto> login(@RequestBody UserLoginReqDto request) {
+        UserAuthResDto response = userService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
