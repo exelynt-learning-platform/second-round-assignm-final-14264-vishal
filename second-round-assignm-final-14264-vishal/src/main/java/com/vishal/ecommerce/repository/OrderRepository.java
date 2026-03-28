7ecea1c6ecde6891b3b99a615f4e37d61cab475a
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 18:58:11 +0530

    add OrderRepository

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/repository/OrderRepository.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/repository/OrderRepository.java
@@ -1,5 +1,14 @@
 package com.vishal.ecommerce.repository;
 
-public class OrderRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vishal.ecommerce.entity.Order;
+import com.vishal.ecommerce.entity.User;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUser(User user);
     
 }
