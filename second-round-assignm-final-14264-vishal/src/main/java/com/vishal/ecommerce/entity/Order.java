1b4d3f81ec223271b853cf51a6a22b0158af0cca
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 18:41:49 +0530

    Created Order entity

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/entity/Order.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/entity/Order.java
@@ -1,5 +1,46 @@
 package com.vishal.ecommerce.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+        name = "order_products",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    private Double totalPrice;
+
+    private String shippingAddress;
+
+    private String paymentStatus;
+
+    private LocalDateTime createdAt;
     
 }
