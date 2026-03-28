e6c47c7bac802a875ea382aa1f7e6c465d2fd872
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 17:25:17 +0530

    add CartController with cart management endpoints

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/controller/CartController.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/controller/CartController.java
@@ -1,5 +1,60 @@
 package com.vishal.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vishal.ecommerce.dto.req.CartItemReqDto;
+import com.vishal.ecommerce.dto.res.CartResDto;
+import com.vishal.ecommerce.service.CartService;
+
+@RestController
+@RequestMapping("/api/cart")
 public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    private String getUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+
+    @GetMapping
+    public ResponseEntity<CartResDto> getCart() {
+        CartResDto response = cartService.getCart(getUsername());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<CartResDto> addItem(@RequestBody CartItemReqDto request) {
+        CartResDto response = cartService.addItem(getUsername(), request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/items/{itemId}")
+    public ResponseEntity<CartResDto> updateItem(@PathVariable Long itemId, @RequestBody Integer quantity) {
+        CartResDto response = cartService.updateItem(getUsername(), itemId, quantity);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/items/{itemId}")
+    public ResponseEntity<String> removeItem(@PathVariable Long itemId) {
+        cartService.removeItem(getUsername(), itemId);
+        return ResponseEntity.ok("Item removed");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> clearCart() {
+        cartService.clearCart(getUsername());
+        return ResponseEntity.ok("Cart cleared");
+    }
     
 }
