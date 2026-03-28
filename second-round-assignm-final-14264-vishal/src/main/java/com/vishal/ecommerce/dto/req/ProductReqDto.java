1d03eedac6c8b789f51a7097882b52fcb6f37923
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 12:25:20 +0530

    add Product request and response DTOs

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/req/ProductReqDto.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/req/ProductReqDto.java
@@ -1,5 +1,12 @@
 package com.vishal.ecommerce.dto.req;
 
+import lombok.Data;
+
+@Data
 public class ProductReqDto {
-    
+
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
 }
--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/res/ProductResDto.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/res/ProductResDto.java
@@ -1,5 +1,14 @@
 package com.vishal.ecommerce.dto.res;
 
+import lombok.Data;
+
+@Data
 public class ProductResDto {
-    
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stock;
+
 }
