38871d7952c6d051460e4a841e8bd5f60776508c
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 22:18:21 +0530

    add Order request and response DTOs

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/req/OrderReqDto.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/req/OrderReqDto.java
@@ -1,5 +1,9 @@
 package com.vishal.ecommerce.dto.req;
 
+import lombok.Data;
+
+@Data
 public class OrderReqDto {
-    
+ 
+    private String shippingAddress;
 }
--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/res/OrderResDto.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/dto/res/OrderResDto.java
@@ -1,5 +1,18 @@
 package com.vishal.ecommerce.dto.res;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class OrderResDto {
+
+    private Long id;
+    private List<String> productNames;
+    private Double totalPrice;
+    private String shippingAddress;
+    private String paymentStatus;
+    private LocalDateTime createdAt;
     
 }
