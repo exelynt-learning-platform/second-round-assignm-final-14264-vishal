c5bc3fb34d9ba96788ad790d7e65e5af5429dd44
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 14:08:55 +0530

    added ProductController with CRUD endpoints

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/controller/ProductController.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/controller/ProductController.java
@@ -1,5 +1,52 @@
 package com.vishal.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.vishal.ecommerce.dto.req.ProductReqDto;
+import com.vishal.ecommerce.dto.res.ProductResDto;
+import com.vishal.ecommerce.service.ProductService;
+
+import java.util.*;
+
 public class ProductController {
     
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResDto> addProduct(@RequestBody ProductReqDto request) {
+        ProductResDto response = productService.addProduct(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResDto>> getAllProducts() {
+        List<ProductResDto> response = productService.getAllProducts();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResDto> getProductById(@PathVariable Long id) {
+        ProductResDto response = productService.getProductById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResDto> updateProduct(@PathVariable Long id, @RequestBody ProductReqDto request) {
+        ProductResDto response = productService.updateProduct(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.ok("Product deleted successfully");
+    }
 }
