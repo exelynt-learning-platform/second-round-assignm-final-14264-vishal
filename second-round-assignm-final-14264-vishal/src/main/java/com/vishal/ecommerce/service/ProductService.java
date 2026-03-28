6f57102522a4f60f748f2d13e15bab76ded11958
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-28 12:40:47 +0530

    add ProductService interface with methods we need

--- second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/service/ProductService.java
+++ second-round-assignm-final-14264-vishal/src/main/java/com/vishal/ecommerce/service/ProductService.java
@@ -1,5 +1,21 @@
 package com.vishal.ecommerce.service;
 
-public class ProductService {
-    
+import com.vishal.ecommerce.dto.req.ProductReqDto;
+import com.vishal.ecommerce.dto.res.ProductResDto;
+
+import java.util.*;
+
+public interface ProductService {
+
+        ProductResDto addProduct(ProductReqDto request);
+
+            List<ProductResDto> getAllProducts();
+
+                ProductResDto getProductById(Long id);
+
+                    ProductResDto updateProduct(Long id, ProductReqDto request);
+
+                        void deleteProduct(Long id);
+
+
 }
