b3ee8dff64441e5615a183a7b82479dab930d752
vishal7651 <vishalkshirsagar992@gmail.com>
2026-03-30 16:54:02 +0530

    chore: remove unused CartReqDto file

--- second-round-assignm-final-14264-vishal/src/test/java/com/vishal/ecommerce/EcommerceApplicationTests.java
+++ second-round-assignm-final-14264-vishal/src/test/java/com/vishal/ecommerce/EcommerceApplicationTests.java
@@ -3,7 +3,7 @@ package com.vishal.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "stripe.secret.key=sk_test_dummy_key_for_testing")
 class EcommerceApplicationTests {
 
 	@Test
